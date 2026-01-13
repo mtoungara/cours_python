71b5f6de2aff02e060ff62c342b19129d7175ed1
Toungara <toungara@it-computing.net>
2026-01-13 16:44:06 +0100

    format3

---
+++ format3.py
@@ -0,0 +1,19 @@
+phrase = "Apprendre Python"
+# Affichage de nombre de caractère 
+print(len(phrase))
+
+# Affichage de caractère
+print(phrase[0])
+
+# Afficher le 6eme element 
+print(phrase[5])
+
+# Test de chaine appartenance 
+
+print("p" in phrase)
+
+# Test chaine non appartenance
+
+print("App" not in phrase)
+
+print("em" not in phrase)
