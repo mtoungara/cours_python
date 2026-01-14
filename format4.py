d6873134671f35bfacb3fb6d7ec74cdd4b4d4ca6
Toungara <toungara@it-computing.net>
2026-01-14 13:49:41 +0100

    fonction

---
+++ format4.py
@@ -0,0 +1,12 @@
+phrase = "Apprendre Python"
+# element 2 -> 4 est exclus 0 est inclus
+print(phrase[0:4])
+# nombre de pas -> 3
+print(phrase[0:14:3])
+# Du debut à la fin
+print(phrase[::2])
+#slicing et les indices négatifs
+print(phrase[-1])
+print(phrase[-13:-5])
+print(phrase[-3:-12:-2])
+
---
+++ script_function_input.py
@@ -0,0 +1,37 @@
+longueur = 10
+largeur = 7
+perimetre = (longueur+largeur)*2
+air = longueur*largeur
+
+print(perimetre, air)
+print("Le périmetre du rectangle est: ",perimetre)
+print("L'air du rectangle est: ",air)
+
+#input l'utilisateur doit saisir un caractere 
+
+longueur = input()
+largeur = input()
+
+perimetre = (longueur+largeur)*2
+print("Le périmetre du rectangle est: ",perimetre)
+
+# utilisation de float 
+
+longueur = float(input())
+largeur = float(input())
+
+perimetre = (longueur + largeur)*2
+air = longueur * largeur
+
+print("Le perimetre du rectangle est: ",perimetre)
+print("L'air du rectangle est: ",air)
+
+# Message de saisi
+
+longueur = float(input("Veuillez saisir la longueur du rectangle:"))
+
+largeur = float(input("Veuillez saisir la largeur du rectangle:"))
+perimetre = (longueur + largeur)*2
+air = longueur * largeur
+print("Le perimetre du rectangle est: ",perimetre)
+print("L'air du rectangle est: ",air)
