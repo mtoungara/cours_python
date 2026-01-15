d2699d9b02bbd4aadef81dc1d19c4a4c17476a70
Toungara <toungara@it-computing.net>
2026-01-15 17:56:45 +0100

    variables booleens

---
+++ var_booleens.py
@@ -0,0 +1,24 @@
+dossier_selectionne = True
+if dossier_selectionne:
+    print("envoyer une convocation au candidat pour audition")
+else:
+    print("dossier non selectionne")
+#
+dossier_selectionne = False  
+if dossier_selectionne:
+    print("envoyer une convocation au candidat pour audition")
+else:
+    print("dossier non selectionne")
+#
+
+dossier_selectionne = False  
+if not(dossier_selectionne):
+    print("dossier non selectionne")
+else:
+    print("envoyer une convocation au candidat pour audition")
+#
+dossier_selectionne = True 
+if not(dossier_selectionne):
+    print("dossier non selectionne")
+else:
+    print("envoyer une convocation au candidat pour audition")
