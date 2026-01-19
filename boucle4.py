907cddd689a286503a66fd96622a0fb8f4719866
Toungara <toungara@it-computing.net>
2026-01-19 17:08:49 +0100

    boucle

---
+++ boucle4.py
@@ -0,0 +1,3 @@
+expression = "Python est un language formidable"
+for c in expression:
+    print(c)
---
+++ boucle5.py
@@ -0,0 +1,13 @@
+for n in range(10):
+    print(n)
+
+#
+for n in range(10,100,5):
+    print(n)
+#
+
+for n in range(101):
+    if n%2 == 0:
+        print("Le nombre: ",n, "est un nombre impair")
+    else:
+        print("Le nombre: ",n, " est un nombre impair")
